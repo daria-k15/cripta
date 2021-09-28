b16e2fd65e01af77f1b20b6bba22295427ba02e4
daria_k <dariakuptsova15@gmail.com>
2021-09-28 17:29:02 +0300

    5th and 6th tasks

--- 1_lab/Lab1.java
+++ 1_lab/Lab1.java
@@ -1,7 +1,5 @@
 // import java.util.Random;
 import java.util.Scanner;
-
-
 public class Lab1{
     public static void main(String[] args){
         Scanner vvod = new Scanner(System.in);
@@ -15,6 +13,10 @@ public class Lab1{
         //     zadanie3();
 		else if (n == 4)
 			zadanie4();
+		else if (n == 5)
+			zadanie5();
+		else if (n == 6)
+			zadanie6();
 		else if (n == 7)
 			zadanie7();
         else
@@ -151,47 +153,23 @@ public class Lab1{
                 else
                 {
                     if (word[i] + n < 65)
-                        encrypt_w[i] = (char)(word[i] + n + 26);
+                        encrypt_w[i] = (char)(word[i] + n + 32);
                     else
                         encrypt_w[i] = (char)(word[i] + n);
                 }
             }
             else if (word[i] > 96 && word[i] < 123)
             {
-                if (word[i] + n > 123)
+                if (word[i] + n > 122)
                     encrypt_w[i] = (char)(word[i] + n - 26);
                 else
 				{
 					if(word[i] + n < 97)
-						encrypt_w[i] = (char)(word[i] + n + 26);
+						encrypt_w[i] = (char)(word[i] + n + 32);
 					else
 						encrypt_w[i] = (char)(word[i] + n);
 				}
             }
-            // else if (word[i] > 1071 && word[i] < 1104)
-            // {
-            //     if (word[i] + n > 1103)
-            //         encrypt_w[i] = (char)(word[i] + n - 32);
-            //     else
-			// 	{
-			// 		if(word[i] + n < 1072)
-            //         	encrypt_w[i] = (char)(word[i] + n + 32);
-			// 		else
-			// 			encrypt_w[i] = (char)(word[i] + n);
-			// 	}
-            // }
-            // else if (word[i] > 1039 && word[i] < 1072)
-            // {
-            //     if (word[i] + n > 1071)
-            //         encrypt_w[i] = (char)(word[i] + n - 32);
-            //     else
-			// 	{
-			// 		if (word[i] < 1039)
-            //         	encrypt_w[i] = (char)(word[i] + n + 32);
-			// 		else
-			// 			encrypt_w[i] = (char)(word[i] + n);
-			// 	}
-            // }
             else
                 encrypt_w[i] = word[i];
             i++;
@@ -219,43 +197,6 @@ public class Lab1{
         vvod_int.close();
 
     }
-
-	// public static String encrypte_3(String str)
-	// {
-	// 	String res = "";
-    //     int i = 0;
-    //     int len = str.length();
-    //     // char[] word = new char[len];
-    //     // while (i < len)
-    //     // {
-    //     //     word[i] = str.charAt(i);
-    //     //     i++;
-    //     // }
-    //     // char[] encrypt_w = new char[len];
-    //     // i = 0;
-    //     Random k = new Random();
-	// 	// byte[] byte = new byte[];
-	// 	while (i < len)
-	// 	{
-	// 		if(str == " ")
-	// 			res += 256 * k.nextInt(1) + 4;
-	// 		else
-	// 		{
-	// 			byte[] byte = Encoding.GetEncoding(1251).GetBytes(str.substring(i, 1));
-				
-	// 		}
-	// 	}        
-	// }
-	
-    // public static void zadanie3()
-    // {
-    //     Scanner vvod = new Scanner(System.in);
-    //     System.out.print("Задание 3\n");
-    //     System.out.print("Введите сообщение, которое нужно зашифровать\n");
-    //     String str = vvod.nextLine();
-    //     encrypt_3(str);
-	// 	vvod.close();
-    // }
 
 		static int check(String str, char a)
 		{
@@ -331,48 +272,60 @@ public class Lab1{
 			j = j_found(word[i], array);
 			k1 = i_found(word[i + 1], array);
 			j1 = j_found(word[i + 1], array);
-
+            System.out.println("k = " + k);
+            System.out.println("j = " + j);
+            System.out.println("k1 = " + k1);
+            System.out.println("j1 = " + j1);
 			if (k == 0 && j == 0)
-				word[i] = ' ';
+                i++;
+            // else if ((k == 0 && j == 0) && (k1 != 0 || j1 != 0))
+            //     i++;
             else if (k > k1){
                 if (j == 3 && j1 == 4){
                     word[i] = array[k][j + 1];
                     word[i + 1] = array[k1][0];
+                    i+= 2;
                 }
-                // else if (j == 4)
-                //     word[i] = array[k][0];
+                else if ((j == 0 && k == 0) ||(j1 == 0 && k1 == 0))
+                    i++;
                 else{
                     word[i] = array[k][j + 2];
                     word[i + 1] = array[k1][j1 - 2];
+                    i+= 2;
                 }
             }
             else {
                 if (j == 3 && j1 == 4){
                     word[i] = array[k][j + 1];
                     word[i + 1] = array[k1][0];
+                    i+= 2;
                 }
                 else{
                 word[i] = array[k][j + 2];
-                word[i + 1] = array[k1][j1 - 2];}
+                word[i + 1] = array[k1][j1 - 2];
+                i+= 2;}
+                
             }
             System.out.printf("%c", word[i]);
+            System.out.printf("%c", word[i+1]);
 			// else if (j + 2 < 5)
 			// 	word[i] = array[k][j + 2];
 			// else
 			// 	word[i] = array[k][j - 2];
-			i+= 2;
+			// i+= 2;
 		}
 		str = String.valueOf(word);
 		return (str);
 	}
 
+    
 	public static String decrypt_4(String str, char[][] array, int len)
 	{
-		char[] word = new char[str.length()];
+        char[] word = new char[str.length()];
 		int i = 0;
 		while (i < len)
 		{
-			word[i] = str.charAt(i);
+            word[i] = str.charAt(i);
 			i++;
 		}
 		i = 0;
@@ -381,30 +334,123 @@ public class Lab1{
 		i = 0;
 		while (i < len)
 		{
-			k = i_found(word[i], array);
+            k = i_found(word[i], array);
 			j = j_found(word[i], array);
 			if (k == 0 && j == 0)
-				word[i] = ' ';
+            word[i] = ' ';
             // else if (j + 2 <= 4)
             //     word[i] = array[k][j + 2];
 			else if (j - 2 > 0)
-				word[i] = array[k][j - 2];
+            word[i] = array[k][j - 2];
             else if (j - 2 == 0)
             word[i] = array[k][j - 2];
 			else
-				word[i] = array[k][j + 2];
+            word[i] = array[k][j + 2];
 			i++;
 		}
 		str = String.valueOf(word);
 		return (str);
 	}
+    public static String lol(String str, char[][] array)
+    {
+        char[]word = new char[str.length()];
+        int i = 0;
+		int j = 0;
+        // char[][] encr = new char[str.length()/2][2];
+        while (i < str.length())
+        {
+			if (str.charAt(i) == ' ')
+				i++;
+			else{
+            word[j] = str.charAt(i);
+            j++;
+			i++;
+		}	
+            
+        }
+        i = 0;
+		str = String.valueOf(word);
+        int k;
+        int k1, j1;
+		
+       while (i < str.length() - 1)
+	   {
+		   k = i_found(word[i], array);
+		   j = j_found(word[i], array);
+		   k1 = i_found(word[i+1], array);
+		   j1 = j_found(word[i+1], array);
+		//    if ((k == 0 && j == 0) || (k1 == 0 && j1 == 0))
+		   		// i++;
+		  	if (j1 == j)
+		   {
+			   word[i] = array[k][j + 2];
+			   word[i+1] = array[k1][j1+2];
+			   i = i+2;
+			}
+			else
+			{
+				word[i] = array[k][j1];
+				word[i + 1] = array[k1][j];
+				i = i + 2;
+			}
+		}
+			String res = String.valueOf(word);
+			System.out.println(res);
+			return(res);
+    }
+    
+	public static String lol_d(String str, char[][] array, String val)
+	{
+		char[]word = new char[str.length()];
+        int i = 0;
+		// char[] kek = new char[val.length()];
+		// while (i < val.length())
+		// {
+		// 	kek[i] = val.charAt(i);
+		// 	i++;
+		// }
+		// i=0;
+        while (i < str.length())
+        {
+            word[i] = str.charAt(i);
+            i++;
+            
+        }
+        i = 0;
+        int k, j;
+        int k1, j1;
+       while (i < str.length() - 1)
+	   {
+		   k = i_found(word[i], array);
+		   j = j_found(word[i], array);
+		   k1 = i_found(word[i+1], array);
+		   j1 = j_found(word[i+1], array);
+		   if ((k == 0 && j == 0) || (k1 == 0 && j1 == 0))
+		   		i++;
+		   if (j1 == j)
+		   {
+			   word[i] = array[k][j - 2];
+			   word[i+1] = array[k1][j1 - 2];
+			   i = i + 2;
+			}
+			else
+			{
+				word[i] = array[k][j1];
+				word[i + 1] = array[k1][j];
+				i = i + 2;
+			}
+			}
+			String res = String.valueOf(word);
+			System.out.println(res);
+			return (res);
+	}
 
 	public static void zadanie4(){
-		Scanner vvod = new Scanner(System.in);
+        Scanner vvod = new Scanner(System.in);
 		System.out.println("4th task\n");
 		System.out.println("Enter a message\n");
 		String str = vvod.nextLine();
-		String key = "table";
+		String key = "cable";
 		int len = str.length();
 		// static int flag;
 		char[][] array = new char[5][5];
@@ -440,27 +486,57 @@ public class Lab1{
 			}
 			i++;
 		}
-		// if (str.length() % 2 == 0)
-		str = encrypt_4(str, array, len);
-		System.out.println(str);
-		// System.out.println(flag);
-		// str = decrypt_4(str, array, len);
-		// System.out.println(str);
-		// else
-		// 	System.out.println("Even sentence\n");
-		// i = 0;
-		// while (i < 5)
-		// {
-		// 	int j = 0;
-		// 	while (j < 5)
-		// 	{
-		// 		System.out.printf("%c  ", array[i][j]);
-		// 		j++;
-		// 	}
-		// 	System.out.printf("\n");
-		// 	i++;
-		// }
+        i = 0;
+        while (i < 5)
+        {
+           int j = 0;
+            while (j < 5)
+            {
+                System.out.printf("%c  ", array[i][j]);
+                j++;
+            }
+            System.out.println();
+            i++;
+        }
+		System.out.printf("len 1= %d\n", str.length());
+		
+		String encr = lol(str, array);
+		System.out.printf("len 2= %d\n", encr.length());
+		
+		String dec = lol_d(encr, array, str);
+		System.out.printf("len 3= %d\n", dec.length());
+
 		vvod.close();
+	}
+
+	public static void zadanie5()
+	{
+		Scanner vvod_str = new Scanner(System.in);
+        Scanner vvod_int = new Scanner(System.in);
+        System.out.print("5th task\n");
+        System.out.print("Enter a shift\n");
+        int n = vvod_int.nextInt();
+        System.out.print("Enter a message\n");
+        String str = vvod_str.nextLine();
+        System.out.printf("\n");
+        str = encrypt_2(str, n);
+        System.out.println(str);
+        str = encrypt_2(str, -n);
+        System.out.println(str);
+        vvod_str.close();
+        vvod_int.close();
+
+	}
+
+	public static void zadanie6()
+	{
+		System.out.println("6th task\n");
+		String str = "Pbatenghyngvbaf! Vg'f n Pnrfne pvcure!";
+		for (int i = 0; i < 26; i++)
+		{
+			str = encrypt_2(str, i);
+			System.out.println(i + "." + str);
+		}
 	}
 
 	public static void zadanie7(){
@@ -469,7 +545,6 @@ public class Lab1{
 		System.out.println("Введите сообщение, которое нужно зашифровать\n");
 		String str = vvod.nextLine();
 		str = encrypt_2(str, 13);
-		// str = encrypte_2(str, 13);
 		System.out.println(str);
 		str = encrypt_2(str, -13);
 		System.out.println(str);
